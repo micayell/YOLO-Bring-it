28369a933a6fc8f7709dfe57dfea6c28a06c12fc
김창주 <micayell98@naver.com>
2026-09-07 14:05:46 +0900

    chore: 사용하지 않는 레거시 UI 및 미니 게임(Dead Code) 컴포넌트 대거 삭제 및 파일 정리

--- frontend/src/domains/game/components/game/index.ts
+++ frontend/src/domains/game/components/game/index.ts
@@ -1,11 +1,7 @@
 export { GameCountdownScreen } from './GameCountdownScreen';
-export { GameCompleteScreen } from './GameCompleteScreen';
 export { GameHeader } from './GameHeader';
-export { ParticipantGrid } from './ParticipantGrid';
-export { VideoPlayer } from './VideoPlayer';
 
 export { GameTimer } from './GameTimer';
-export { GameMainContent } from './GameMainContent';
 
 // 게임 컴포넌트들
 export * from './games';
